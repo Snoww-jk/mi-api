2e2e6cd61b2ba9f4f19b4446a23bd79e937ca0fe
Snoww-jk <fr.aguirrer@duocuc.cl>
2026-09-01 18:13:42 -0400

    Primera version de mi API

---
+++ src/main/java/com/duoc/miapi/MiApiApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.miapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/miapi/MiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.miapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
